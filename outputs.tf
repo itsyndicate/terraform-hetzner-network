332d3b74943570c62756a701d59c0c8acf5b0449
Alex K <7284572+cSkyHawk@users.noreply.github.com>
2025-02-06 22:21:03 -0500

    Adds subnets output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,12 @@ output "network_id" {
   description = "ID of the Network resource"
   value       = hcloud_network.this.id
 }
+
+output "public_subnets" {
+  value = {
+    for key, subnet in local.public_subnets : key => {
+      name = key
+      ip_range = subnet.ip_range
+    }
+  }
+}
